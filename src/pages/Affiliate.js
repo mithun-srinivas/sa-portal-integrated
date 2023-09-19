00a269d79108cb333a9d4d8659aec1cccbea5b1a
Mithun.S <mithunsrinivasappa@gmail.com>
2023-09-19 21:50:06 +0530

    Updated

--- src/pages/Affiliate.js
+++ src/pages/Affiliate.js
@@ -202,7 +202,7 @@ const Affiliate = () => {
                             <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
                                 <h2 className="text-3xl font-bold leading-tight text-black sm:text-4xl">Join Our Affiliate Program.</h2>
 
-                                <form action="https://api.sheetmonkey.io/form/uvcbL8tTSCozu8AhyaSeNx" method="POST" className="mt-8">
+                                <form onSubmit={notify} action="https://api.sheetmonkey.io/form/uvcbL8tTSCozu8AhyaSeNx" method="POST" className="mt-8">
                                     <div className="space-y-5">
                                         <div>
                                             <label htmlFor="" className="text-base font-medium text-gray-900">
@@ -240,7 +240,6 @@ const Affiliate = () => {
                                         <div>
                                             <button
                                                 type="submit"
-                                                onClick={notify}
                                                 className="inline-flex w-full items-center justify-center rounded-md bg-[#001732] px-3.5 py-2.5 font-semibold leading-7 text-white hover:bg-[#001732]/80"
                                             >
                                                 Start Earning<ArrowRight className="ml-2" size={16} />
